710dddcc69faeefc4d30b81fb0a8e030a28ee015
JTarazona00 <jtarazonaf2@upao.edu.pe>
2026-07-14 10:39:34 -0500

    chekov N4

--- iac/vpc.tf
+++ iac/vpc.tf
@@ -123,3 +123,61 @@ resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
 }
+
+# --- Fix CKV2_AWS_11: flow logs de la VPC ----------------------------------
+# Registra todo el tráfico de la VPC en CloudWatch Logs (cifrado con la CMK
+# de logs), para auditoría y diagnóstico de red.
+resource "aws_cloudwatch_log_group" "vpc_flow" {
+  name              = "/vpc/${var.project}/flow-logs"
+  retention_in_days = 30
+  kms_key_id        = aws_kms_key.logs.arn
+}
+
+data "aws_iam_policy_document" "vpc_flow_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["vpc-flow-logs.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "vpc_flow" {
+  name               = "${var.project}-vpc-flow-logs"
+  assume_role_policy = data.aws_iam_policy_document.vpc_flow_assume.json
+}
+
+data "aws_iam_policy_document" "vpc_flow" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams"
+    ]
+
+    resources = ["${aws_cloudwatch_log_group.vpc_flow.arn}:*"]
+  }
+}
+
+resource "aws_iam_role_policy" "vpc_flow" {
+  name   = "${var.project}-vpc-flow-logs"
+  role   = aws_iam_role.vpc_flow.id
+  policy = data.aws_iam_policy_document.vpc_flow.json
+}
+
+resource "aws_flow_log" "main" {
+  vpc_id               = aws_vpc.main.id
+  traffic_type         = "ALL"
+  iam_role_arn         = aws_iam_role.vpc_flow.arn
+  log_destination_type = "cloud-watch-logs"
+  log_destination      = aws_cloudwatch_log_group.vpc_flow.arn
+
+  tags = {
+    Name = "${var.project}-vpc-flow-logs"
+  }
+}
