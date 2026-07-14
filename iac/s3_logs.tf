0eda93c66b2368791f9cc56dabd53326268456d7
JTarazona00 <jtarazonaf2@upao.edu.pe>
2026-07-14 00:53:26 -0500

    chekov N1

--- iac/s3_logs.tf
+++ iac/s3_logs.tf
@@ -25,3 +25,12 @@ resource "aws_s3_bucket_public_access_block" "logs" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+# --- Fix CKV_AWS_21: versionado del bucket --------------------------------
+resource "aws_s3_bucket_versioning" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
